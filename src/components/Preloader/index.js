a91cff4eb710bcc4750b0e023a296f00b37c174b
Максим Подлесный <volkovmaks2100@gmail.com>
2023-11-01 01:40:52 +0200

    fix

--- src/components/Preloader/index.js
+++ src/components/Preloader/index.js
@@ -3,9 +3,9 @@ import './style.scss';
 
 const Preloader = () => {
   return (
-      <div className="preloader-container">
-        <img src={Logo} alt="Logo" />
-      </div>
+    <div className="preloader-container">
+      <img src={Logo} alt="Logo" />
+    </div>
   );
 };
 
