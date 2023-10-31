16868440b0f7f38abccc690e5c52ff4f08039e4f
Максим Подлесный <volkovmaks2100@gmail.com>
2023-11-01 01:39:21 +0200

    fix

--- src/components/Preloader/index.js
+++ src/components/Preloader/index.js
@@ -1,11 +1,10 @@
-import Logo from './logo.svg';
+import Logo from './logos.svg';
 import './style.scss';
 
 const Preloader = () => {
   return (
     <div className="preloader-container">
       <div className="flex-container">
-        <h1>Bureaux</h1>
         <img src={Logo} alt="Logo" />
       </div>
     </div>
